55de519fde71cbe04b4a31bde255a377f8a3f7ab
chebee7i <chebee7i@gmail.com>
2012-05-08 15:45:31 -0700

    Removing functions provided by Vrui.

--- DataItem.cpp
+++ DataItem.cpp
@@ -18,7 +18,7 @@
  along with the Dynamics Toolset. If not, see <http://www.gnu.org/licenses/>.
  *******************************************************************************/
 #include "DataItem.h"
-
+#include <iostream>
 // Vrui includes
 //
 #include <Misc/ThrowStdErr.h>
@@ -34,116 +34,6 @@
 
 namespace DTS
 {
-
-//
-// Shader utility methods
-//
-
-namespace
-{
-
-/*********************************************************
- Support functions to compile/link vertex/fragment shaders:
- *********************************************************/
-
-void glCompileShaderFromString(GLhandleARB shaderObject, const char* shaderSource)
-{
-   /* Determine the length of the source string: */
-   GLint shaderSourceLength=GLint(strlen(shaderSource));
-
-   /* Upload the shader source into the shader object: */
-   const GLcharARB* ss=reinterpret_cast<const GLcharARB*> (shaderSource);
-   glShaderSourceARB(shaderObject, 1, &ss, &shaderSourceLength);
-
-   /* Compile the shader source: */
-   glCompileShaderARB(shaderObject);
-
-   /* Check if the shader compiled successfully: */
-   GLint compileStatus;
-   glGetObjectParameterivARB(shaderObject, GL_OBJECT_COMPILE_STATUS_ARB, &compileStatus);
-   if (!compileStatus)
-   {
-      /* Get some more detailed information: */
-      GLcharARB compileLogBuffer[2048];
-      GLsizei compileLogSize;
-      glGetInfoLogARB(shaderObject, sizeof(compileLogBuffer), &compileLogSize, compileLogBuffer);
-
-      /* Signal an error: */
-      Misc::throwStdErr("glCompileShaderFromString: Error \"%s\" while compiling shader", compileLogBuffer);
-   }
-}
-
-GLhandleARB glCompileVertexShaderFromString(const char* shaderSource)
-{
-   /* Create a new vertex shader: */
-   GLhandleARB vertexShaderObject=glCreateShaderObjectARB(GL_VERTEX_SHADER_ARB);
-
-   try
-   {
-      /* Load and compile the shader source: */
-      glCompileShaderFromString(vertexShaderObject, shaderSource);
-   } catch (std::runtime_error err)
-   {
-      /* Clean up and re-throw the exception: */
-      glDeleteObjectARB(vertexShaderObject);
-
-      throw ;
-   }
-
-   return vertexShaderObject;
-}
-
-GLhandleARB glCompileFragmentShaderFromString(const char* shaderSource)
-{
-   /* Create a new vertex shader: */
-   GLhandleARB fragmentShaderObject=glCreateShaderObjectARB(GL_FRAGMENT_SHADER_ARB);
-
-   try
-   {
-      /* Load and compile the shader source: */
-      glCompileShaderFromString(fragmentShaderObject,shaderSource);
-   }
-   catch(std::runtime_error err)
-   {
-      /* Clean up and re-throw the exception: */
-      glDeleteObjectARB(fragmentShaderObject);
-
-      throw;
-   }
-
-   return fragmentShaderObject;
-}
-
-GLhandleARB glLinkShader(GLhandleARB vertexShaderObject,GLhandleARB fragmentShaderObject)
-{
-   /* Create the program object: */
-   GLhandleARB programObject=glCreateProgramObjectARB();
-
-   /* Attach all shader objects to the shader program: */
-   glAttachObjectARB(programObject,vertexShaderObject);
-   glAttachObjectARB(programObject,fragmentShaderObject);
-
-   /* Link the program: */
-   glLinkProgramARB(programObject);
-
-   /* Check if the program linked successfully: */
-   GLint linkStatus;
-   glGetObjectParameterivARB(programObject,GL_OBJECT_LINK_STATUS_ARB,&linkStatus);
-   if(!linkStatus)
-   {
-      /* Get some more detailed information: */
-      GLcharARB linkLogBuffer[2048];
-      GLsizei linkLogSize;
-      glGetInfoLogARB(programObject,sizeof(linkLogBuffer),&linkLogSize,linkLogBuffer);
-
-      /* Signal an error: */
-      Misc::throwStdErr("glLinkShader: Error \"%s\" while linking shader program",linkLogBuffer);
-   }
-
-   return programObject;
-}
-
-} // namespace::
 
 //
 // DataItem methods
