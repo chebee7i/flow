972b9eaba25379effa8427566795dd0111714039
chebee7i <chebee7i@gmail.com>
2012-05-16 11:14:22 -0700

    Typo in differential equations for Owl.

--- dynamics/src/Dynamics/plugins/Owl.h
+++ dynamics/src/Dynamics/plugins/Owl.h
@@ -16,9 +16,9 @@ public:
         name = "Owl";
 
         double inf = std::numeric_limits<Scalar>::infinity();
-        addCoordinate( Coordinate("x", .5, -25, 25) );
+        addCoordinate( Coordinate("x", .5, -10, 10) );
         addCoordinate( Coordinate("y", .5, -10, 10) );
-        addCoordinate( Coordinate("z", .5, -1, 40) );
+        addCoordinate( Coordinate("z", .5, -10, 10) );
         addCoordinate( Coordinate("t", 0, 0, inf) );        
 
         addRealParameter( RealParameter("a", a, -20, 20,  10, .01) );
@@ -30,7 +30,7 @@ public:
 
     virtual void operator()(Vector const& p, Vector & out) const
     {
-        out[0] = realParamValues[0] * (p[0] + p[1]);
+        out[0] = -realParamValues[0] * (p[0] + p[1]);
         out[1] = -p[1] - realParamValues[1] * p[0] * p[2];
         out[2] = 10 * p[0] * p[1] + realParamValues[2];
         out[3] = 1;
