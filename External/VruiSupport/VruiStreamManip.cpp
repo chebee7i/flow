75b78820a93005b747ba953a85313c263433500b
chebee7i <chebee7i@gmail.com>
2012-07-12 15:03:12 -0700

    Deleting DTS

--- External/VruiSupport/VruiStreamManip.cpp
+++
@@ -1,8 +0,0 @@
-/*******************************************************************************
- VruiStreamManip: Classes for restricting output by render node.
- Copyright (c) 2006-2008 Jordan Van Aalsburg
- *******************************************************************************/
-#include <iostream>
-#include "VruiStreamManip.h"
-
-bool debug::filter::_debug=false;
